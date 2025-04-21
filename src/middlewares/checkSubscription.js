fda6767b5d618ef5e25a20796b10df313273c8a5
DouglasMoraesDev <doougmooraes2024@gmail.com>
2025-04-21 20:13:18 -0300

    teste 18

--- src/middlewares/checkSubscription.js
+++ src/middlewares/checkSubscription.js
@@ -1,5 +1,5 @@
 // src/middlewares/checkSubscription.js
-const { prisma } = require('./prisma/schema.prisma'); // ajuste o caminho conforme necessário
+const { prisma } = require('../../config/db'); // ✅ caminho corrigido
 
 async function checkSubscription(req, res, next) {
   const estId = req.user.establishmentId; // supondo que o auth já tenha populado req.user
